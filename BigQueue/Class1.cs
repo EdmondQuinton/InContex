ae619d9865fe741bbc69dde19aa0dd7c0a24cd2a
Edmond Quinton <edmond.quinton@live.com>
2018-03-05 12:28:56 -0500

    Revert "Removed legacy test code."
    This reverts commit c3d8e83583d9d34a88c0b31ccbbd11e411512b47.

---
+++ BigQueue/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigQueue
+{
+    public class Class1
+    {
+    }
+}
---
+++ InfoHubService/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using InContex.Service.Configuration;
+using NLog;
+
+namespace InContex.Service
+{
+    static class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if(args != null)
+            {
+                foreach(string arg in args)
+                {
+                    string argument = arg.ToUpperInvariant().Trim();
+
+                    switch(argument)
+                    {
+                        case "-C":
+                            AppRuntimeConfiguration.RuntimeMode = AppRuntimeType.Console;
+                            break;
+                    }
+                }
+            }
+
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new InfoHubService()
+            };
+
+            logger.Info("Application runtime mode set \"{0}\"", AppRuntimeConfiguration.RuntimeMode.ToString());
+
+            if (AppRuntimeConfiguration.RuntimeMode != AppRuntimeType.Console)
+            {
+                logger.Info("Starting service...");
+                ServiceBase.Run(ServicesToRun);
+            }
+            else
+            {
+                logger.Info("Starting console...");
+                InfoHubService consoleService = ServicesToRun[0] as InfoHubService;
+                consoleService.StartConsole(args);
+
+                Console.WriteLine("Press 'Q' to quit ...");
+                char key = Console.ReadKey(true).KeyChar;
+
+                while(key != 'Q' && key != 'q')
+                {
+                    key = Console.ReadKey(true).KeyChar;
+                }
+
+                logger.Info("Stopping Console...");
+                consoleService.StopConsole();
+            }
+        }
+    }
+}
